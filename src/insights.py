5abc07726f460076a5bd2b1bb87eafa90b56f8ca
EzequielVerissimoCarvalhoZica <ezeverissimo@gmail.com>
2022-07-01 16:24:29 -0300

    feat: req 04

--- src/insights.py
+++ src/insights.py
@@ -55,21 +55,14 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
+    data = read(path)
+    salaries = [
+        int(salary["max_salary"])
+        for salary in data
+        if salary["max_salary"].isnumeric()
+    ]
 
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    return max(salaries)
 
 
 def get_min_salary(path):
